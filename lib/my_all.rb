06b00c32cb6eca7e87699d4ac9cba31b0ec1a22a
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-19 19:23:21 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-  is_true? = TRUE
+  is_true = TRUE
   i = 0
   while i < collection.assert_recognizes
     if (yield(collection[i]) == nil || yield(collection[i]) == false)
-      is_true? = FALSE
+      is_true = FALSE
     end
     i += 1
   end
